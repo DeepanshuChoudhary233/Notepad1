aaad9d8badacbee9f5c937ca94cf61c955532a35
DeepanshuChoudhary233 <deepmagma103@gmail.com>
2025-11-02 14:06:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const express=require('express');
+const fs=require('fs');
+const app=express();
+const path=require('path');
+app.set('view-engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/',function(req,res){
+    fs.readdir(`./files`,function(err,files){
+        res.render("index.ejs",{files:files});//object k roop mein iss index page p kuch bhi bhej skte hain 
+    })
+});
+app.get('/files/:filename',function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render("show.ejs",{filename:req.params.filename,filedata:filedata});
+    })
+});
+app.get('/edit/:filename',function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render("edit.ejs",{filename:req.params.filename,filedata:filedata});
+    })
+});
+app.post('/edit',function(req,res){
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+        res.redirect(`/`);
+    });
+});
+app.post('/create',function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+        res.redirect("/");
+    });
+});
+app.post('/delete',function(req,res){
+  fs.unlink(`./files/${req.body.filename}`,function(err){
+    res.redirect("/");
+  })
+});
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
